c4d38ef879774e88e69bbe3447c0950c4ab48787
csecsu-web <csenge200815@gmail.com>
2025-11-15 23:12:59 +0100

    Update vendeghaz.js

--- vendeghaz.js
+++ vendeghaz.js
@@ -1,25 +1,23 @@
-// Scroll animáció
+// Fade-in scroll
 const faders = document.querySelectorAll('.fade-slide');
-const footer = document.querySelector('footer');
-
-const appearOptions = { threshold: 0.2, rootMargin:"0px 0px -50px 0px" };
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+const options = { threshold: 0.2 };
+const appearOnScroll = new IntersectionObserver(function(entries, observer){
     entries.forEach(entry => {
         if(entry.isIntersecting){
             entry.target.classList.add('active');
+        } else {
+            entry.target.classList.remove('active');
         }
     });
-}, appearOptions);
+}, options);
 
 faders.forEach(fader => appearOnScroll.observe(fader));
 
 // Footer fade-in
-const footerObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            footer.style.opacity = 1;
-        }
+const footer = document.querySelector('footer');
+const footerObserver = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){ footer.style.opacity = 1; }
     });
-}, {threshold:0.1});
-
+},{threshold:0.1});
 footerObserver.observe(footer);
